56f5e47926ba3aa81fb6e946b0ca369447f0f591
Reino Zuppur <reino@reinozuppur.com>
2022-10-30 22:17:20 +0200

    upgrade rvc

--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -161,7 +161,7 @@ const version = ref('');
     .background-error.r-p-md.r-border-radius-md.r-text-color-red.r-text-medium.r-space(v-if="errorText") {{ errorText }}
 
   .r-p-lg(v-else)
-    r-button.r-m-b-md(borderless small :action="resetCollection" icon="arrow left" icon-color="blue") Back
+    r-button.r-m-b-md(gray small :action="resetCollection" icon="arrow_back") Back
     .r-text-sm.r-text-medium {{ collectionFilePath }}
     .r-text-xxs.r-text-color-muted Rekordbox version {{ collectionVersion }} &middot; {{ collectionTracks.length }} tracks &middot;&nbsp;
       a(@click="() => { $refs.playlists.open(); }") {{ collectionPlaylists.length }} playlists
@@ -171,28 +171,28 @@ const version = ref('');
 
     .r-m-t-lg(v-if="collectionTracksNotInPlaylists.length")
       h2.r-text-md.r-text-medium
-        r-icon.red.r-m-r-sm(icon="attention")
+        r-icon.yellow.r-m-r-sm(icon="error_circle_rounded" size="lg")
         | {{ collectionTracksNotInPlaylists.length }} lost track{{ collectionTracksNotInPlaylists.length === 1 ? "" : "s" }}
       .r-text-color-muted.r-m-t-xs These tracks are in collection but weren't found in any playlist. You can save these tracks to a m3u8 playlist that can be imported to Rekordbox. There you can either add them to a playlist or delete from collection.
       r-button.r-m-t-md(primary :action="actionSaveLostPlaylist" :loading="playlistSaving" icon="download") Export tracks
     h2.r-text-md.r-text-medium.r-m-t-lg(v-else)
-      r-icon.green.r-m-r-sm(icon="check")
+      r-icon.green.r-m-r-sm(icon="check" size="lg")
       | All collection tracks are in playlists
 
     .r-m-t-lg(v-if="collectionPlaylistDuplicates.size")
       h2.r-text-md.r-text-medium
-        r-icon.red.r-m-r-sm(icon="attention")
+        r-icon.yellow.r-m-r-sm(icon="error_circle_rounded" size="lg")
         | {{ collectionPlaylistDuplicates.size }} playlist{{ collectionPlaylistDuplicates.size === 1 ? " has" : "s have" }} duplicate tracks
       .r-m-t-sm(v-for="playlist in collectionPlaylistDuplicates")
         .r-text-medium {{ playlist[0] }}
         .r-text-color-muted(v-for="track in playlist[1]") &bullet; {{ track }}
     h2.r-text-md.r-text-medium.r-m-t-lg(v-else)
-      r-icon.green.r-m-r-sm(icon="check")
+      r-icon.green.r-m-r-sm(icon="check" size="lg")
       | No duplicates in playlists
 
     .r-m-t-lg(v-if="collectionTracksProbableDuplicates.length")
       h2.r-text-md.r-text-medium
-        r-icon.red.r-m-r-sm(icon="attention")
+        r-icon.yellow.r-m-r-sm(icon="error_circle_rounded" size="lg")
         | {{ collectionTracksProbableDuplicates.length }} probable duplicate tracks
       .r-text-color-muted.r-m-t-xs You can save these tracks to a m3u8 playlist that can be imported to Rekordbox. There you can delete one of the files from collection.
       .r-buttons.r-m-t-md.r-m-b-md
@@ -205,7 +205,7 @@ const version = ref('');
               span.r-text-medium {{ formatSeconds(track.TotalTime) }}
               span.r-text-color-muted &nbsp;{{ cleanLocationString(track.Location).replace("file://localhost/", "") }}
     h2.r-text-md.r-text-medium.r-m-t-lg(v-else)
-      r-icon.green.r-m-r-sm(icon="check")
+      r-icon.green.r-m-r-sm(icon="check" size="lg")
       | No duplicates in collection
 
 .r-text-color-muted.r-p-sm.r-text-xxs Rekordfix v{{ version }}
